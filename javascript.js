00c7d592914db8c9e5427eb0b663865f91e316b3
pravinamalan <pravinamalan21@gmail.com>
2022-12-16 16:35:32 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,16 @@
+var loader = document.getElementById('preloader');
+
+        window.addEventListener('load', function(){
+            loader.style.display='none';
+        })
+
+
+// popup
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
